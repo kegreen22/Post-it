9bc74cd6c04775c31e6f69ae17a38e8f98ea39f4
Keith Green <kegreen22@gmail.com>
2015-01-06 23:20:45 -0500

    1-6-15

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,48 @@
 class PostsController < ApplicationController
   def index
-
+  	@posts = Post.all 
+    # show all posts
   end
+
+  
+def new
+   @post = Post.new  # new Post form
+end
+
+def create
+   @post = Post.new(post_params)
+   if @post.save
+   flash[:notice] = "Your Post has been created. Thanks!"
+   redirect_to posts_path
+   # Render new template again if database save fails (an error)
+   else
+   render :new
+  end
+end
+
+
+def show
+  	@post = Post.find(params[:id])
+end
+ 
+def edit
+   @post = Post.find(params[:id])
+end
+
+def update
+   @post = Post.find(params[:id])
+   if @post.update(post_params)
+   flash[:notice] = "Your Post has been updated. Thanks!"
+   redirect_to posts_path
+   # Render edit template again if database save fails (an error)
+   else
+   render :edit
+   end
+end
+
+ 
+def post_params
+   params.require(:post).permit!
+end
+
 end
