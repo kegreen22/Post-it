e400eb7094eca915137650b141b4108796654b17
Keith Green <kegreen22@gmail.com>
2015-01-15 19:59:43 -0500

    1-15-15b

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
   resources :posts, except: [:destroy] do
   	resources :comments, only: [:create]
-
+end
   resources :categories, only: [:new, :show, :create]
 end
