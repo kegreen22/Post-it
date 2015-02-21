ad8ab78fa9c1570075ffc859fb98a49046e1caf2
Keith Green <kegreen22@gmail.com>
2015-02-20 19:41:54 -0500

    2-20-15 fixed comments controller/view errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,7 +18,7 @@ end
 
 def vote
 comment = Comment.find(params[:id])
-vote = Vote.create(voteable: comment, user: current_user, vote: params([:vote]))
+vote = Vote.create(voteable: comment, user: current_user, vote: params[:vote])
 if vote.valid?
 flash[:notice]="Your vote was counted."
 else
