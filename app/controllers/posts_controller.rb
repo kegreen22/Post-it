3ade30e535937aae6d98754ed09ed4987d3760da
Keith Green <kegreen22@gmail.com>
2015-03-17 23:04:08 -0400

    3-17-15 fixed nil error in posts/categories; only allow admin to edit posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-before_action :set_post, only: [:show, :edit, :update, :vote]
 before_action :require_user, except: [:show, :index] #require_user is application-wide & is a redirect if not logged in
+before_action :set_post, only: [:show, :edit, :update, :vote]
 before_action :require_admin, only: [:edit]
 
   def index
